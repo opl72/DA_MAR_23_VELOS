c55a384c4be32cb7e277c044e5c06381451da425
opl72 <146993873+opl72@users.noreply.github.com>
2023-10-04 21:41:33 +0200

    Update Velo.py

--- Velo.py
+++ Velo.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-
-import streamlit as st
-st.write("Bonjour, ceci est votre projet Streamlit !")
-
-
 import streamlit as st
 import pandas as pd
 import numpy as np
