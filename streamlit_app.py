d4f10820a989ebd84e912dd03f8271c09317cdae
opl72 <146993873+opl72@users.noreply.github.com>
2023-10-05 10:04:15 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,9 +6,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-import calendar
-import locale
 #import plotly_express as px
+
 
 @st.cache_data
 def load_df():
@@ -17,15 +16,14 @@ def load_df():
 	return df_group_par_j_2023, df_predict_2023
 	
 @st.cache_data
-def plot_site_2023(df_src, df_pred, Mois, nom_compteur) :
-	mois = calendar.month_name[Mois].capitalize()    
-	df_site_src = df_src[(df_src.Mois == Mois) & (df_src.nom_compteur == nom_compteur)]
+def plot_site_2023(df_src, df_pred, mois, numero_mois, nom_compteur) :
+	df_site_src = df_src[(df_src.Mois == numero_mois) & (df_src.nom_compteur == nom_compteur)]
 			
 	fig, ax = plt.subplots(figsize = (20,7))   
 	# données relevées
 	ax.plot(df_site_src.Jour, df_site_src.sum_counts, 'b-', label='comptages réels')
 	# prédictions
-	df_site_pred = df_pred[(df_pred.Mois == Mois) & (df_pred["site_"+nom_compteur] == 1)]
+	df_site_pred = df_pred[(df_pred.Mois == numero_mois) & (df_pred["site_"+nom_compteur] == 1)]
 	ax.plot(df_site_pred.Jour, df_site_pred.sum_counts, 'r-', label='prédictions')
 	
 	# affichage des jours du mois
@@ -37,35 +35,40 @@ def plot_site_2023(df_src, df_pred, Mois, nom_compteur) :
 	plt.grid(True)
 	plt.legend()
 	st.pyplot(fig)
-
-try:
-	 # affichage des mois en francais
-	locale.setlocale(locale.LC_TIME, 'fr_FR.UTF-8')
-	st.write(locale.getlocale())
-except locale.Error:
-    	# Si la locale n'est pas disponible sur le système, traitez l'exception ici
-   	st.write("La locale 'fr_FRA.UTF-8' n'est pas disponible sur ce système.")
 	
 st.title("Analyse du trafic cycliste à Paris")
 st.sidebar.title("Sommaire")
-pages = ["Dataset principal", "Datasets secondaires", "Exploration", "DataVisualization", "Modélisation"]
+pages = ["Dataset principal", 
+		 "Datasets secondaires", 
+		 "Explorations", 
+		 "Data Viz", 
+		 "ML & Prédictions", 
+		 "Conclusion et perspectives"]
 page = st.sidebar.radio("Aller vers", pages)
+st.sidebar.markdown("<br><b>Auteurs : </b><br>Cécile ALBET<br> Olivier PELLETEY", unsafe_allow_html=True)
+st.sidebar.markdown("<br>Formation Data Analyst<br>Promotion Mars 2023", unsafe_allow_html=True)
+st.sidebar.image("logoDS.png")
+
 
 if page == pages[0] : 
-	st.write("### Dataset principal")   
-	st.write("1. Source: Le jeu de données provient du site opendata.paris.")     
-	st.write("La Ville de Paris déploie depuis plusieurs années des compteurs vélo permanents  (site ou point de comptage) pour évaluer le développement de la pratique cycliste. Les compteurs sont situés sur des pistes cyclables et dans certains couloirs bus ouverts aux vélos. Les autres véhicules (ex : trottinettes…) ne sont pas comptés.")
-	st.write("Remarque : Le nombre de compteurs évolue au fur et à mesure des aménagements cyclables. Certains compteurs peuvent être désactivés pour travaux ou subir ponctuellement une panne.")
+	st.header("Dataset principal")
+	
+	st.markdown("<u>Source :</u><br>Le jeu de données provient du site : [opendata.paris.fr](https://opendata.paris.fr/explore/dataset/comptage-velo-donnees-compteurs/information/?disjunctive.id_compteur&disjunctive.nom_compteur&disjunctive.id&disjunctive.name)", unsafe_allow_html=True)    
+	
+	st.markdown('<p style="text-align: justify;"><br>La Ville de Paris déploie depuis plusieurs années des compteurs vélo permanents  (site ou point de comptage) pour évaluer le développement de la pratique cycliste. Les compteurs sont situés sur des pistes cyclables et dans certains couloirs bus ouverts aux vélos. Les autres véhicules (ex : trottinettes…) ne sont pas comptés.</p>', unsafe_allow_html=True)
+	
+	st.markdown('<p style="text-align: justify;"><u>Remarque :</u><br> Le nombre de compteurs évolue au fur et à mesure des aménagements cyclables. Certains compteurs peuvent être désactivés pour travaux ou subir ponctuellement une panne.</p>', unsafe_allow_html=True)
 
 if page == pages[4] : 	
-	st.write("### Modélisation")
-	st.write("### Prédictions du trafic 2023")
+	
+	st.header("Prédictions du trafic 2023")
 	df_group_par_j_2023, df_predict_2023 = load_df()
 	liste_sites = df_group_par_j_2023.nom_compteur.unique()
 	site = st.selectbox('Sélectionnez un site de comptage :', liste_sites)
 	
-	liste_mois = df_group_par_j_2023.Mois.unique()	
-	liste_mois_cap = [calendar.month_name[mois].capitalize() for mois in liste_mois]
-	mois = st.selectbox('Sélectionnez le mois à prédir :', liste_mois_cap)	
-	index_du_mois = liste_mois_cap.index(mois.capitalize()) + 1
-	plot_site_2023(df_group_par_j_2023, df_predict_2023, index_du_mois, site) 
+	liste_mois = ['Janvier', 'Février', 'Mars', 'Avril']
+	#liste_mois_cap = [calendar.month_name[mois].capitalize() for mois in liste_mois]
+	mois = st.selectbox('Sélectionnez le mois à prédir :', liste_mois)	
+	numero_mois = liste_mois.index(mois.capitalize()) + 1
+	
+	plot_site_2023(df_group_par_j_2023, df_predict_2023, mois, numero_mois, site) 
