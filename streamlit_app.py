fa0e97f3a84cd589a15eba19ef037eea910b6213
opl72 <146993873+opl72@users.noreply.github.com>
2023-10-07 16:04:57 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,13 +4,14 @@ import matplotlib.pyplot as plt
 from streamlit_option_menu import option_menu
 import extra_streamlit_components as stx
 
-# Config par defaut de l'appli
+# CONFIG DE L'APPARENCE DE L'APPLI
 st.set_page_config(layout="wide", # affichage par défaut en mode wide
 				   page_title="Trafic cycliste parisien", # titre de l'appli dans la barre du navigateur
-				   initial_sidebar_state = "collapsed",
+				   initial_sidebar_state = "collapsed", # apparence de la barre latérale
 				   page_icon=":bike:") # icone de l'appli dans la barre du navigateur
 
-# mise en cache des ressources à charger
+
+# MISE EN CACHE DES RESSOURCES UTILES
 @st.cache_data
 def load_and_cache(file_path):
 	df = pd.read_csv(file_path)
@@ -41,21 +42,7 @@ def plot_site_2023(df_src, df_pred, mois, numero_mois, nom_compteur) :
 	st.pyplot(fig)
 
 
-
-icons = ['bicycle', 'database', 'binoculars', 'bar-chart-line', 'cpu', 'wrench-adjustable-circle']
-pages = ['Contexte', 'Datasets', 'Explorations', 'DataViz', 'Prédictions', 'Perspectives']
-	
-# SIDEBAR
-# with st.sidebar :
-#     page = option_menu(
-# 							'Sommaire', 
-# 							options=options1,
-# 							icons=icons, 
-# 							menu_icon="menu-up",							
-# 							default_index=0)
-    
-
-# PANEL DE GAUCHE
+# BARRE LATERALE
 image = "logoDS.png"
 st.sidebar.markdown("Formation continue<br>Data Analyst<br>Promotion Mars 2023", unsafe_allow_html=True)	
 st.sidebar.image(image, width=150)
@@ -63,11 +50,13 @@ st.sidebar.markdown("<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br
 st.sidebar.markdown("<b>Auteurs :</b><br>[Cécile ALBET](https://fr.linkedin.com/in/c%C3%A9cile-albet-322593143)<br>Olivier PELLETEY", unsafe_allow_html=True)
 
 
-
 # TITRE
 st.markdown('<p style="text-align:center; font-size:45px; font-weight:bold;">Analyse du trafic cycliste à Paris</p>', unsafe_allow_html=True)
 
+
 # MENU HORIZONTAL
+icons = ['bicycle', 'database', 'binoculars', 'bar-chart-line', 'cpu', 'question-diamond']
+pages = ['Contexte', 'Jeux de données', 'Explorations', 'DataViz', 'Machine Learning', 'Perspectives']
 page = option_menu(
 				None, 
 				options=pages,
@@ -82,7 +71,6 @@ page = option_menu(
 				      })
 
 
-
 # GESTION DE CHAQUE PAGE
 # PAGE 1
 if page == pages[0] : 
@@ -92,31 +80,36 @@ if page == pages[0] :
 	
 # PAGE 2
 if page == pages[1] : 
-	st.title("Jeux de données")
-	st.write("---")
+	#st.title("Jeux de données")
+	#st.write("---")
 	
+	# SLIDER HORIZONTAL
 	chosen_id = stx.tab_bar(data=[
 		   stx.TabBarItemData(id=1, title="Dataset principal", description=""),
-		   stx.TabBarItemData(id=2, title="Dataset secondaire", description="")
+		   stx.TabBarItemData(id=2, title="Datasets secondaires", description="")
 						   		 ], default=1)
 	#st.info(f"{chosen_id=}")
 
 	if chosen_id == "1" :
 		st.header("Dataset principal")
-	
-		st.markdown("<u>Source :</u><br>Le jeu de données provient du site : [opendata.paris.fr](https://opendata.paris.fr/explore/dataset/comptage-velo-donnees-compteurs/)", unsafe_allow_html=True)    	
+		
+		st.subheader('1. Source')
+		st.markdown("Le jeu de données provient du site : [opendata.paris.fr](https://opendata.paris.fr/explore/dataset/comptage-velo-donnees-compteurs/)", unsafe_allow_html=True)    	
 		st.markdown('<p style="text-align: justify;"><br>La Ville de Paris déploie depuis plusieurs années des compteurs vélo permanents  (site ou point de comptage) pour évaluer le développement de la pratique cycliste. Les compteurs sont situés sur des pistes cyclables et dans certains couloirs bus ouverts aux vélos. Les autres véhicules (ex : trottinettes…) ne sont pas comptés.</p>', unsafe_allow_html=True)	
 		st.markdown('<p style="text-align: justify;"><u>Remarque :</u><br> Le nombre de compteurs évolue au fur et à mesure des aménagements cyclables. Certains compteurs peuvent être désactivés pour travaux ou subir ponctuellement une panne.</p>', unsafe_allow_html=True)
 
-	if chosen_id == "2" :
-		st.header("Dataset secondaire")
 		
+
+	if chosen_id == "2" :
+		st.header("Datasets secondaires")
+		st.subheader('1. Sources')
 		
 # PAGE 3
 if page == pages[2] : 
 	st.title("Explorations / Analyses")
 	st.write("---")
 		
+	# chargement de la carte accidents vélos 2021
 	with open("carte_acc_velos_par_arrond_2021.html", 'r', encoding='utf-8') as f:
 		   fic_html = f.read()
 	st.components.v1.html(fic_html, height=600, width=600)
@@ -153,21 +146,36 @@ if page == pages[3] :
 
 # PAGE 5
 if page == pages[4] : 	
-	st.title("Modélisations")
-	st.write("---")
-	st.header("1. Séries temporelles")
-	st.header("2. Modèles de ML")
-	st.header("3. Prédictions du trafic 2023")
+	#st.title("Modélisations")
+	#st.write("---")
+	
+	# SLIDER HORIZONTAL
+	chosen_id = stx.tab_bar(data=[
+		   stx.TabBarItemData(id=1, title="Séries temporelles", description=""),
+		   stx.TabBarItemData(id=2, title="Modélisations", description=""),
+		   stx.TabBarItemData(id=3, title="Prédictions", description="")
+		   ], default=1)
 
-	liste_sites = df_group_par_j_2023.nom_compteur.unique()
-	site = st.selectbox('Sélectionnez un site de comptage :', liste_sites)
+	if chosen_id == "1" :
+		st.header("Séries temporelles")
+		
+		
+	if chosen_id == "2" :
+		st.header("Modèles de Machine Learning")
+		
+		
+	if chosen_id == "3" :
+		st.header("Prédictions du trafic 2023")	
 	
-	liste_mois = ['Janvier', 'Février', 'Mars', 'Avril']
-	#liste_mois_cap = [calendar.month_name[mois].capitalize() for mois in liste_mois]
-	mois = st.selectbox('Sélectionnez le mois à prédir :', liste_mois)	
-	numero_mois = liste_mois.index(mois.capitalize()) + 1
-	
-	plot_site_2023(df_group_par_j_2023, df_predict_2023, mois, numero_mois, site) 
+		liste_sites = df_group_par_j_2023.nom_compteur.unique()
+		site = st.selectbox('Sélectionnez un site de comptage :', liste_sites)
+		
+		liste_mois = ['Janvier', 'Février', 'Mars', 'Avril']
+		#liste_mois_cap = [calendar.month_name[mois].capitalize() for mois in liste_mois]
+		mois = st.selectbox('Sélectionnez le mois à prédir :', liste_mois)	
+		numero_mois = liste_mois.index(mois.capitalize()) + 1
+		
+		plot_site_2023(df_group_par_j_2023, df_predict_2023, mois, numero_mois, site) 
 	
 	
 # PAGE 6
